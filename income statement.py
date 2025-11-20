58f52e547810209b2d4b433dbb38a636ede500f2
jbrown1251 <joshuabrown@my.ccsu.edu>
2025-11-20 18:50:59 -0500

    Add files via upload

---
+++ income statement.py
@@ -0,0 +1,18 @@
+# declare our constant
+pct=.4
+#input from user
+sales = float(input("enter sales($): "))
+#processing
+#calculate the cost of good sold(cgs)
+
+cgs = sales * cgsoct
+
+#calculate the gross profit
+grossprofit = sales - cgs
+
+#print sales
+
+#print cost of goods sold
+
+#out - Display the gross profit
+print("Gross profit: $'")
